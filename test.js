4ebd14357f40e3a741341aeafd2c1010b76cf6ed
Hiren Joshi <hirenj@gmail.com>
2019-12-03 22:16:24 +0100

    More test

--- test.js
+++ test.js
@@ -20,22 +20,38 @@ const or_geneset = (genes) => {
   return geneset[0];
 };
 
-// Solution should be ['ST3GAL1','ST3GAL2','ST3GAL3','ST3GAL4','ST3GAL6']
+// Solution should be ['ST3GAL3','ST3GAL1' ,'ST3GAL2']
+
+// ST3GAL1/2/3/4/5/6 down
+solver.require(Logic.or('ST3GAL1','ST3GAL2','ST3GAL3','ST3GAL4','ST3GAL5','ST3GAL6'));
+
+// ST3GAL1/2/3 down
+
+solver.require(Logic.or('ST3GAL1','ST3GAL2','ST3GAL3'));
 
 
-solver.forbid('ST6GAL1');
-solver.forbid('ST6GAL2');
-solver.require(Logic.not(and_geneset(['ST6GAL1','ST6GAL2'])));
-solver.require(Logic.not(and_geneset([ 'ST6GAL1', 'ST6GAL2', 'ST3GAL1', 'ST3GAL2', 'ST3GAL5' ])));
-solver.require(Logic.not(and_geneset([ 'ST6GAL1', 'ST6GAL2', 'ST3GAL3', 'ST3GAL4', 'ST3GAL6' ])));
-solver.require(or_geneset([ 'ST6GAL1',
-  'ST6GAL2',
-  'ST3GAL1',
-  'ST3GAL2',
-  'ST3GAL3',
-  'ST3GAL4',
-  'ST3GAL5',
-  'ST3GAL6' ]));
+console.log(solver.solve().getTrueVars());
+
+
+// ST6GAL1/2 no effect
+// IGNORED..
+
+// ST3GAL1/2 no effect
+
+solver.require(Logic.implies(Logic.not(Logic.and('ST3GAL1','ST3GAL2')),Logic.or('ST3GAL3','ST3GAL4','ST3GAL5')));
+
+// ST3GAL3/4/6 no effect
+
+solver.require(Logic.implies(Logic.not(Logic.and('ST3GAL3','ST3GAL4','ST3GAL6')), Logic.or('ST3GAL2','ST3GAL1','ST3GAL5') ) );
+
+solver.require(Logic.not(Logic.or('ST3GAL4' ,'ST3GAL6','ST3GAL3')));
+
+// ST3GAL3/4/6 ST6GAL1/2 no effect
+// IGNORED..
+
+
+console.log(solver.solve().getTrueVars());
+
 let soln = solver.solve();
 var allSolutions = [];
 var curSolution = null;
@@ -44,3 +60,31 @@ while ((curSolution = solver.solve())) {
   solver.forbid(curSolution.getFormula());
 }
 console.log(allSolutions);
+
+
+solver.require(Logic.and( Logic.not(Logic.or('ST3GAL3','ST3GAL4','ST3GAL6')), Logic.or('ST3GAL1','ST3GAL2','ST3GAL5','ST6GAL1','ST6GAL2')) );
+
+console.log(solver.solve().getTrueVars());
+
+
+solver.require(Logic.and( Logic.not(Logic.and('ST3GAL3','ST3GAL4','ST3GAL6','ST6GAL1','ST6GAL2')), Logic.and('ST3GAL1','ST3GAL2','ST3GAL5')) );
+
+
+console.log(solver.solve().getTrueVars());
+
+
+
+solver.require(Logic.and( Logic.not(Logic.or('ST3GAL1','ST3GAL2')), Logic.and('ST3GAL3','ST3GAL4','ST3GAL5','ST3GAL6','ST6GAL1','ST6GAL2')) );
+
+console.log(solver.solve().getTrueVars());
+
+
+
+// let soln = solver.solve();
+// var allSolutions = [];
+// var curSolution = null;
+// while ((curSolution = solver.solve())) {
+//   allSolutions.push(curSolution.getTrueVars());
+//   solver.forbid(curSolution.getFormula());
+// }
+// console.log(allSolutions);
