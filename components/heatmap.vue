2f075614d20a28622dc4cb45c4fb55b8ee8644ab
Hiren Joshi <hirenj@gmail.com>
2019-12-10 13:49:54 +0100

    New patterns for heatmap

--- components/heatmap.vue
+++ components/heatmap.vue
@@ -8,8 +8,8 @@
 
 const PATTERNS = {
 	'B4GALT6' : '*(u?-?)*(u?-?)Gal(u?-?)Glc(u?-?)Cer',
-	'COSMC' : 'Gal(b1-3)GalNAc(a1-O)Ser;*(u?-?)Gal(b1-3)GalNAc(a1-O)Ser;Gal(b1-3)[*(u?-?)GlcNAc(b1-?)]GalNAc(a1-O)Ser',
-	'MGAT1' : '*(u?-?)*(u?-?)GlcNAc(b1-?)Man(a1-?)Man(b1-4)GlcNAc(b1-4)GlcNAc(b1-N)Asn'
+	'COSMC' : 'Gal(b1-3)GalNAc(a1-O)Ser;*(u?-?)*(u?-?)Gal(b1-3)GalNAc(a1-O)Ser;*(u?-?)Gal(b1-3)GalNAc(a1-O)Ser;*(u?-?)Gal(b1-3)GalNAc(a1-O)Ser;Gal(b1-3)[*(u?-?)*(u?-?)GlcNAc(b1-?)]GalNAc(a1-O)Ser',
+	'MGAT1' : 'GlcNAc(b1-4)Man(b1-4)GlcNAc(b1-4)GlcNAc(b1-N)Asn;*(u?-?)*(u?-?)GlcNAc(b1-?)Man(a1-?)Man(b1-4)GlcNAc(b1-4)GlcNAc(b1-N)Asn'
 }
 
 export default {
